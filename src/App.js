b774bf165958055682900325d453b5a6d2966c49
Vidhush-920 <thvidhush@gmail.com>
2024-11-20 00:56:10 +0530

    Created Navbar UI for user homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
+    <>
+    <Navbar />
     <div>
       <h1>Swift Supply</h1>
     </div>
+    </>
+    
   );
 }
 
