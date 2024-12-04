959972c339d6167e429f747f0a891e1dce9347db
Pavithiran Sivaganesh <94287015+Pavithiran2000@users.noreply.github.com>
2024-12-05 00:10:00 +0530

    Revert "Feature/signin and restpasword"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
 import './App.css';
-import ProtectedRoutes from './routes/ProtectedRoutes';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <ProtectedRoutes /> 
-      </Router>
-    </AuthProvider>
+    <div>
+      <h1>Swift Supply</h1>
+    </div>
   );
 }
 
